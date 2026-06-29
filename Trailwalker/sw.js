4bdb42dbc05758bcd50f3cdda99b69eb2406373c
kinlunho <kinlunho16@gmail.com>
2026-06-29 20:36:08 +0800

    Add files via upload

--- Trailwalker/sw.js
+++ Trailwalker/sw.js
@@ -1,23 +1,10 @@
-const CACHE = 'tw2026-v3';
-const ASSETS = ['/', '/index.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+// Unregister all service workers and clear all caches
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request)
-      .then(r => { const c = r.clone(); caches.open(CACHE).then(cache => cache.put(e.request, c)); return r; })
-      .catch(() => caches.match(e.request))
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.registration.unregister())
   );
+  self.clients.claim();
+  self.clients.matchAll().then(clients => clients.forEach(c => c.navigate(c.url)));
 });
